ce96b60bd9336ad317bb1909b7d9f5fefe684373
tripurnomo <tripurnomo.mail@gmail.com>
2022-06-12 11:37:42 +0700

    Update data_ingestion_products.py
    change csv filename

--- data_ingestion_products.py
+++ data_ingestion_products.py
@@ -10,7 +10,7 @@ SQL_DB = os.environ.get('OLTP_DATABASE')
 conn = psycopg2.connect(host=SQL_HOST, dbname=SQL_DB, user=SQL_USER, password=SQL_PASS, port=SQL_PORT)
 cur = conn.cursor()
 sql = 'COPY (select * from products) TO STDOUT WITH CSV HEADER'
-csv_file = open('syarif_products.csv','w')
+csv_file = open('tri_products.csv','w')
 cur.copy_expert(sql,csv_file)
 csv_file.close()
 cur.close
